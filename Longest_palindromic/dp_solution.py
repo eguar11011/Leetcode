d38154592ad04eaa9c65e9060aae22eb75229dbb
eguar11011 <emendezc@unal.edu.co>
2023-08-25 00:34:09 -0400

    Falto subir la carpeta xd

---
+++ Longest_palindromic/dp_solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = [0, 0]
+        # Colocamos en True los caracteres individuales que en realidad todos son palindromos 
+        for i in range(n):
+            dp[i][i] = True
+        # Ahora asignamos los palindromos con centro par (abba)
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                ans = [i, i + 1]
+
+        for diff in range(2, n):
+            for i in range(n - diff):
+                j = i + diff
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    ans = [i, j]
+
+        i, j = ans
+        return s[i:j + 1]
+    
+s = Solution()
+print(s.longestPalindrome("babbad")) 
